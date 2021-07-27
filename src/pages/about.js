4398512bfba427be15e0339d589c7daaa8583d8b
immaroot <killiankelly@gmail.com>
2021-07-27 15:56:27 -0400

    Part 2 of Gatsby Tutorial. Added .idea to gitignore

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import {Link} from "gatsby";
+import Layout from "../components/layout"
+
+const Greetings = (props) => {
+    return (
+        <h2>Hi {props.name}</h2>
+    )
+}
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Page">
+            <p>A page that explains a little bit about myself</p>
+            <Greetings name="Killian" />
+            <Greetings name="Pops" />
+        </Layout>
+    )
+}
+
+export default AboutPage
