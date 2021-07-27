d7bca6919f208b57e6460475ecabf4cb1b1b7078
immaroot <killiankelly@gmail.com>
2021-07-27 16:35:51 -0400

    Part 3 of Gatsby Tutorial. Switched from npm to yarn to fix dep issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
-import {Link} from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
       <Layout pageTitle="Home Page">
           <p>This is the Home Page!</p>
+          <StaticImage
+              src="../images/cat.webp"
+              alt="An Image Of A Cute Cat"
+          />
       </Layout>
   )
 }
